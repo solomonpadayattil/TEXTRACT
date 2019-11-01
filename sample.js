a5b0b0e90ed0f436abe108c3b8d16fd7f994420b
solomonpadayattil <padayattilsolomon@gmail.com>
2019-11-01 18:17:28 +0530

    extracted text from a png image

--- sample.js
+++ sample.js
@@ -1,3 +1,8 @@
+
+const AWS = require('aws-sdk');
+AWS.config.update({
+  region: "us-east-1" //Here add you region
+});
 /*
  * Copyright 2013. Amazon Web Services, Inc. All Rights Reserved.
  * 
@@ -15,22 +20,54 @@
 **/
 
 // Load the SDK and UUID
-var AWS = require('aws-sdk');
-var uuid = require('node-uuid');
+const uuid = require('node-uuid');
 
 // Create an S3 client
-var s3 = new AWS.S3();
+const s3 = new AWS.S3();
+
+
+const express = require('express')
+const app = express()
+const port = 3000
+const textract = new AWS.Textract();
+
+console.log("Region: ", AWS.config.region);
+
+app.get('/textdetect', (req, res) =>{
+
+
+  let params = {
+    Document: { /* required */
+      // Bytes: Buffer.from('...') || 'STRING_VALUE' /* Strings will be Base-64 encoded on your behalf */,
+      S3Object: {
+        Bucket: 'node-sdk-sample-b684ad3c-8362-4b3a-9ccd-54ddc428ba3a',
+        Name: 'testing2.png',
+        // Version: 'STRING_VALUE'
+      }
+    }
+  };
+
+  textract.detectDocumentText(params, function(err, data) {
+    if (err) console.log(err, err.stack); // an error occurred
+    else     console.log(data);           // successful response
+  });
+  // res.send('Hello World!')
+
+})
 
 // Create a bucket and upload something into it
-var bucketName = 'node-sdk-sample-' + uuid.v4();
-var keyName = 'hello_world.txt';
+// var bucketName = 'node-sdk-sample-' + uuid.v4();
+// var keyName = 'hello_world.txt';
 
-s3.createBucket({Bucket: bucketName}, function() {
-  var params = {Bucket: bucketName, Key: keyName, Body: 'Hello World!'};
-  s3.putObject(params, function(err, data) {
-    if (err)
-      console.log(err)
-    else
-      console.log("Successfully uploaded data to " + bucketName + "/" + keyName);
-  });
-});
+// s3.createBucket({Bucket: bucketName}, function() {
+//   var params = {Bucket: bucketName, Key: keyName, Body: 'Hello World!'};
+//   s3.putObject(params, function(err, data) {
+//     if (err)
+//       console.log(err)
+//     else
+//       console.log("Successfully uploaded data to " + bucketName + "/" + keyName);
+//   });
+// });
+
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
